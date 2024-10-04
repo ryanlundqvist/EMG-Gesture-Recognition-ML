c7f136895eb30366f9c8ef24e475b91ca587fef4
Ryan Lundqvist <45742684+ryanlundqvist@users.noreply.github.com>
2024-10-04 19:43:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write(
     "Put the proposal here, you can use headers for formatting or anything you want (Docs: https://docs.streamlit.io/)."
 )
 
-st.subheader("Gantt Chart")
+st.subheader("Gantt Chart", divider="blue")
 st.image("4641_gantt.png")
 st.subheader("Contributions")
 st.image("4641_proposal_contributions.png")
