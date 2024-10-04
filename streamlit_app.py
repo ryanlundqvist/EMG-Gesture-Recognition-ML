5a1533862feb9a8dcf1e5e89535c662366e54416
Ryan Lundqvist <45742684+ryanlundqvist@users.noreply.github.com>
2024-10-04 19:40:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🤖 Machine Learning | EMG | G29")
-st.header("Proposal:")
+st.header("Proposal:", divider="blue")
 st.write(
     "Put the proposal here, you can use headers for formatting or anything you want (Docs: https://docs.streamlit.io/)."
 )
